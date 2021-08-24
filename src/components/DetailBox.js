9f53c8dcb74d37917efb6c9d7eca4ab660b535ca
BuildTools <clayloneman@gmail.com>
2021-08-23 22:12:33 -0400

    more detail

--- src/components/DetailBox.js
+++ src/components/DetailBox.js
@@ -3,10 +3,16 @@ const DetailBox = ({ title, content, link }) => {
     <div className="my-2 flex flex-col dark:text-gray-50 text-gray-800">
       <div className="flex justify-between">
         <h1 className="font-bold">{title}</h1>
-        <h1>Copy</h1>
+        <h1 className="cursor-pointer">Copy</h1>
       </div>
       <div>
-        <a href={link} target="_blank" alt="Share link" rel="noreferrer">
+        <a
+          href={link}
+          className="hover:text-gray-700 dark:hover:text-gray-300"
+          target="_blank"
+          alt="Share link"
+          rel="noreferrer"
+        >
           {link}
         </a>
         <p>{content}</p>
