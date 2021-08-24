3953f07687e0cb231118a69d621d1b4da5c27b36
BuildTools <clayloneman@gmail.com>
2021-08-23 22:09:27 -0400

    fix spacing

--- src/components/AccountDetails.js
+++ src/components/AccountDetails.js
@@ -1,8 +1,8 @@
 import DetailBox from "./DetailBox";
 const AccountDetails = () => {
   return (
-    <div className="px-2 my-2 rounded bg-gray-200 dark:bg-gray-700 text-lg">
-      <div className="border-b-2 border-gray-300 dark:border-gray-800">
+    <div className="px-3 m-2 rounded bg-gray-200 dark:bg-gray-700 text-lg">
+      <div className="border-b-2 border-gray-300 dark:border-gray-600">
         <h1 className="dark:text-gray-50 text-gray-800 font-bold py-2">
           Minecraft Profile
         </h1>
